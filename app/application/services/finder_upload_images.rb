4aac43d054fcd18c6ba00e22cabbe905d00c41b8
tony <tony6liu@gmail.com>
2024-01-05 18:11:43 +0800

    sometimes can work,sometimes not in heroku

--- app/application/services/finder_upload_images.rb
+++ app/application/services/finder_upload_images.rb
@@ -17,6 +17,7 @@ module PetAdoption
       def validate_input(input)
         if input.success?
           input = input.to_h.transform_keys(&:to_s)
+          puts 'validate input success'
           Success(input)
         else
           Failure(input.errors.to_h)
@@ -24,13 +25,15 @@ module PetAdoption
       end
 
       def upload_image(input)
+        puts file_path
         file_path = input['file']
         s3 = PetAdoption::Storage::S3.new
         base_url, object = PetAdoption::Storage::S3.object_url(file_path)
         PetAdoption::Storage::S3.upload_image_to_s3(file_path)
+        puts 'upload image success'
         s3.make_image_public(object)
         input['file'] = "#{base_url}/#{object}"
-
+        puts 'upload image success'
         Success(input:)
       rescue StandardError => e
         Failure(e.message)
@@ -38,6 +41,7 @@ module PetAdoption
 
       def find_the_vets(input)
         res = PetAdoption::Gateway::Api.new(PetAdoption::App.config).recommend_some_vets(input[:input])
+        puts 'vets can be found here'
         Success(res)
       rescue StandardError
         Failure('Sorry, in this moment, there is no vet nearby you')
@@ -45,6 +49,7 @@ module PetAdoption
 
       def reify_vets(results)
         vets = Representer::VetRecommeandation.new(OpenStruct.new).from_json(results.payload)
+        puts 'vets can be created here'
         Success(vets)
       rescue StandardError
         Failure('Error in parsing vets')
--- app/application/services/keeper_upload_images.rb
+++ app/application/services/keeper_upload_images.rb
@@ -16,6 +16,7 @@ module PetAdoption
 
       def validate_input(input)
         if input.success?
+          puts 'validate input success'
           Success(input.to_h.transform_keys(&:to_s))
         else
           Failure(input.errors.to_h)
@@ -23,15 +24,17 @@ module PetAdoption
       end
 
       def upload_image(input)
+        puts file_path
         file_path = input['file']
         s3 = PetAdoption::Storage::S3.new
         base_url, object = PetAdoption::Storage::S3.object_url(file_path)
 
         return Failure('S3 cant upload your image') unless PetAdoption::Storage::S3.upload_image_to_s3(file_path)
 
+        puts 'image upload success'
         s3.make_image_public(object)
         input['file'] = "#{base_url}/#{object}"
-
+        puts 'set image public success'
         Success(input)
       rescue StandardError => e
         Failure(e.message)
@@ -39,7 +42,7 @@ module PetAdoption
 
       def create_keeper_info(input)
         keeper = Gateway::Api.new(PetAdoption::App.config).contact_finders(input)
-
+        puts 'keeper can be found here'
         Success(keeper)
       rescue StandardError
         Failure('Sorry, in this moment, there is no lost animal nearby you')
@@ -47,6 +50,7 @@ module PetAdoption
 
       def reify_keeper(results)
         keeper = Representer::PotentialFinderRepresenter.new(OpenStruct.new).from_json(results.payload)
+        puts 'keeper can be created here'
         Success(keeper)
       rescue StandardError
         Failure('Error in parsing keeper')
